8c92da1643184e71d1d3232be731aa21cc3f7fcf
gauravkharel <kgaurav21@tbc.edu.np>
2021-12-10 11:28:06 +0545

    final commit with GIF

---
+++ app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.hellotoast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int mCount;
+    private TextView mShowCount;
+    private Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mShowCount = (TextView) findViewById(R.id.showCount);
+        button = (Button) findViewById(R.id.zero_btn);
+    }
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void countUp(View view) {
+        mCount++;
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
+        if (mCount != 0)
+            button.setBackgroundColor(Color.GREEN);
+    }
+
+    public void goZero(View view) {
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(0));
+            mCount = 0;
+    }
+
+
+
+}
